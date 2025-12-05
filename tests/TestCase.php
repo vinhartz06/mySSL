e025d273e160fdf4f4e941055e534e581ffa4d30
vinhartz06 <j.vincent2006@gmail.com>
2025-12-05 15:04:20 +0700

    edit testcase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations with seeders for tests
+        $this->artisan('migrate:fresh --seed');
+    }
 }
